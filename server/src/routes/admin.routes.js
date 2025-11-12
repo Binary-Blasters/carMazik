8dc7382b44007f6695ed24d546477ef91bb7ae09
tufail101 <modreneducation479@gmail.com>
2025-11-12 23:07:52 +0530

    Finnal Commit All Done

--- server/src/routes/admin.routes.js
+++ server/src/routes/admin.routes.js
@@ -21,7 +21,7 @@ router.route("/users/:user_id").get(adminController.getUserById)  //checked
 
 //cars 
 router.route("/cars/pending").get(adminController.getPendingCars)  //checked
-router.route("/cars/approve/:car_id").patch(adminController.approveCar) 
-router.route("/cars/reject/:car_id").patch(adminController.rejectCar) 
+router.route("/cars/approve/:car_id").patch(adminController.approveCar)  //checked
+router.route("/cars/reject/:car_id").patch(adminController.rejectCar) //checked
 
 export default router
